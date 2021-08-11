682670294d7129d0cc5c6c9adcf97b102203977b
Šarūnas Navickas <zaibacu@gmail.com>
2021-08-11 18:38:11 +0300

    Game of Life in C

--- game-of-life/README.md
+++ game-of-life/README.md
@@ -1,3 +1,11 @@
-# About
+# Apie
 
-Implementation of (Game of Life)[https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life]
+Suprogramuotas (Game of Life)[https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life]
+
+Video: (Youtube)[https://www.youtube.com/watch?v=V3-bh6ES6Lc]
+
+# Turinys
+
+- `main.py` Game of Life ant Python. Nereikia jokių bibliotekų, tiesiog `python3 main.py`
+
+- `game_of_life.c` tas pats Game Of Life tik parašytas C kalba (kad įrodyt, jog kalba nesvarbi) 
---
+++ game-of-life/game_of_life.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define WIDTH 90
+#define HEIGHT 30
+#define DENSITY 10
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+
+typedef enum { DEAD, ALIVE } state;
+
+void print_grid(state grid[HEIGHT][WIDTH]){
+    for(int y=0; y<HEIGHT; y++){
+        for(int x=0; x<WIDTH; x++){
+            if(grid[y][x] == DEAD){
+                printf(" ");
+            }
+            else {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void init_grid(state grid[HEIGHT][WIDTH]){
+    srand(time(0));
+    for(int y=0; y<HEIGHT; y++){
+        for(int x=0; x<WIDTH; x++){
+            int g = rand() % 100;
+            if(g <= DENSITY){
+                grid[y][x] = ALIVE;
+            }
+            else {
+                grid[y][x] = DEAD;
+            }
+        }
+    }
+}
+
+void clear_screen(){
+    #ifdef _WIN32
+    system("cls");
+    #else
+    system("clear");
+    #endif
+}
+
+void copy_data(state a[HEIGHT][WIDTH], state b[HEIGHT][WIDTH]){
+    for(int y=0; y<HEIGHT; y++){
+        for(int x=0; x<WIDTH; x++){
+            b[y][x] = a[y][x];
+        }
+    }
+}
+
+int sum_alive(state grid[HEIGHT][WIDTH], int ox, int oy){
+    int sum = 0;
+    for(int dx=-1; dx<2; dx++){
+        for(int dy=-1; dy<2; dy++){
+            if(dy == 0 && dx == 0){
+                continue;
+            }
+            int x = ox + dx;
+            int y = oy + dy;
+            if(grid[y][x] == ALIVE){
+                sum++;
+            }
+        }
+    }
+
+    return sum;
+}
+
+void evolution(state orig_grid[HEIGHT][WIDTH]){
+    state new_grid[HEIGHT][WIDTH];
+    copy_data(orig_grid, new_grid);
+
+    // Stuff
+    for(int y=0; y<HEIGHT; y++){
+        for(int x=0; x<WIDTH; x++){
+            int n = sum_alive(orig_grid, x, y);
+            if(orig_grid[y][x] == ALIVE){
+                if(n < 2 || n > 3){
+                    new_grid[y][x] = DEAD;
+                }
+            }
+            else {
+                if(n == 3){
+                    new_grid[y][x] = ALIVE;
+                }
+            }
+        }
+    }
+
+    copy_data(new_grid, orig_grid);
+}
+
+
+int main(int argc, char* argv[]){
+    state grid[HEIGHT][WIDTH];
+    init_grid(grid);
+
+    while(1){
+        clear_screen();
+        print_grid(grid);
+        sleep(1);
+        evolution(grid);
+    }
+    return 0;
+}
